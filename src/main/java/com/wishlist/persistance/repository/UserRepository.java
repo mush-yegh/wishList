8e2f45a031d4f0c325ed28cbddc4a0dfceecb7bd
mush-yegh <mush.yegh@gmail.com>
2019-02-05 16:54:58 +0400

    Extends JpaRepository

--- src/main/java/com/wishlist/persistance/repository/UserRepository.java
+++ src/main/java/com/wishlist/persistance/repository/UserRepository.java
@@ -1,13 +1,18 @@
 package com.wishlist.persistance.repository;
 
 import com.wishlist.persistance.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    //List<UserEntity> findByLastName(String lastName);
+
+    //List<UserEntity> findAllByFirstName();
 
     Optional<UserEntity> findById(Long userId);
+
+    List<UserEntity> findAllByActive(Integer val);
 }
